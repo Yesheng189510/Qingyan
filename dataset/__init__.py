70a74fcf701b2912302be7babc0400968c820187
qiuning <18342144622@163.com>
2026-05-31 02:08:57 +0800

    Remove master branch folders from v2yolo branch

--- dataset/__init__.py
+++
@@ -1,3 +0,0 @@
-from dataset.dataset_processing import DatasetProcessing
-
-__all__ = ['DatasetProcessing']
--- module3_kd/dataset_kd.py
+++
@@ -1,5 +0,0 @@
-"""Dataset wrapper for classification KD. Reuses root DatasetProcessing."""
-
-from dataset.dataset_processing import DatasetProcessing
-
-__all__ = ['DatasetProcessing']
